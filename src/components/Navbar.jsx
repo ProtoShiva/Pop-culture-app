d10efc8048511ac5d7d29812d46d9e8aeaef4824
Himanshu Gusain <himanshugusain9559@gmail.com>
2025-01-03 18:11:43 +0530

    build homepage and its components

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,59 +1,76 @@
 import React from "react"
 import Image from "./Image"
 import LogoutModal from "./LogoutModal"
+import { Link } from "react-router-dom"
+import { useSelector } from "react-redux"
 const Navbar = () => {
+  const isLoggedIn = useSelector((store) => store.user.userDetails)
   return (
-    <div className="navbar flex justify-between">
-      <div className="size-12 object-cover">
-        <Image src="/default-image.jpg" alt="logo" w={32} h={32} />
-      </div>
-      <div>
-        <ul className="flex gap-8">
-          <li>Home</li>
-          <li>Trending</li>
-          <li>Most Popular</li>
-          <li>About</li>
-        </ul>
-      </div>
-      <div className="flex-none">
-        <div className="dropdown dropdown-end">
-          <div
-            tabIndex={0}
-            role="button"
-            className="btn btn-ghost btn-circle avatar"
-          >
-            <div className="w-10 rounded-full">
-              <img
-                alt="Tailwind CSS Navbar component"
-                src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
-              />
-            </div>
-          </div>
-          <ul
-            tabIndex={0}
-            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
-          >
-            <li>
-              <a className="justify-between">
-                Profile
-                <span className="badge">New</span>
-              </a>
-            </li>
-            <li>
-              <a>Settings</a>
-            </li>
-            <li>
-              <a
-                onClick={() =>
-                  document.getElementById("my_modal_2").showModal()
-                }
-              >
-                Logout
-              </a>
-            </li>
+    <div className="navbar flex justify-between items-center px-4 md:px-8 lg:px-16 lx:px-32 2xl:px-64">
+      <div className="flex justify-between w-[63%] items-center">
+        <Link to="/" className="object-cover cursor-pointer">
+          <Image src="/default-image.jpg" alt="logo" w={38} h={38} />
+        </Link>
+        <div>
+          <ul className="flex gap-12">
+            <Link to="/" className="cursor-pointer">
+              Home
+            </Link>
+            <Link to="/" className="cursor-pointer">
+              Trending
+            </Link>
+            <Link to="/" className="cursor-pointer">
+              Most Popular
+            </Link>
+            <Link to="/" className="cursor-pointer">
+              About
+            </Link>
           </ul>
         </div>
       </div>
+
+      {isLoggedIn && (
+        <div className="flex-none mr-3">
+          <div className="dropdown dropdown-end">
+            <div
+              tabIndex={0}
+              role="button"
+              className="btn btn-ghost btn-circle avatar"
+            >
+              <div className="w-10 rounded-full">
+                <img
+                  alt="Tailwind CSS Navbar component"
+                  src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
+                />
+              </div>
+            </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+            >
+              <li>
+                <a className="justify-between">
+                  Profile
+                  <span className="badge">New</span>
+                </a>
+              </li>
+              <li>
+                <a>Settings</a>
+              </li>
+              <li>
+                <a
+                  onClick={() =>
+                    document.getElementById("my_modal_2").showModal()
+                  }
+                >
+                  Logout
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      )}
+
       <LogoutModal />
     </div>
   )
--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,18 @@ import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 import { Provider } from "react-redux"
 import appStore from "./redux/store/appStore.js"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import "./index.css"
 import App from "./App.jsx"
+
+const queryClient = new QueryClient()
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={appStore}>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
     </Provider>
   </StrictMode>
 )
