f78daf9f617e63134fb6ad6debe0abe32f28f495
Himanshu Gusain <himanshugusain9559@gmail.com>
2025-01-04 10:41:26 +0530

    create image upload components

--- src/pages/Write.jsx
+++ src/pages/Write.jsx
@@ -1,14 +1,30 @@
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
 import "react-quill-new/dist/quill.snow.css"
 import ReactQuill from "react-quill-new"
 import { useMutation } from "@tanstack/react-query"
 import client from "../apis/client"
 import { toast } from "react-toastify"
 import { useNavigate } from "react-router-dom"
+import Upload from "../components/Upload"
 
 const Write = () => {
   const [value, setValue] = useState("")
+  const [cover, setCover] = useState("")
+  const [img, setImg] = useState("")
+  const [video, setVideo] = useState("")
+  const [progress, setProgress] = useState(0)
   const navigate = useNavigate()
+
+  useEffect(() => {
+    img && setValue((prev) => prev + `<p><image src="${img.url}"/></p>`)
+  }, [img])
+
+  useEffect(() => {
+    video &&
+      setValue(
+        (prev) => prev + `<p><iframe class="ql-video" src="${video.url}"/></p>`
+      )
+  }, [video])
 
   const mutation = useMutation({
     mutationFn: (newPost) => {
@@ -25,6 +41,7 @@ const Write = () => {
     const formData = new FormData(e.target)
 
     const data = {
+      img: cover.filePath || "",
       title: formData.get("title"),
       category: formData.get("category"),
       desc: formData.get("desc"),
@@ -38,9 +55,11 @@ const Write = () => {
     <div className="h-[calc(100vh-64px)] md:h-[calc(100vh-80px)] flex flex-col gap-6 px-4 md:px-8 lg:px-16 lx:px-32 2xl:px-64">
       <h1 className="text-cl font-light">Create a New Post</h1>
       <form onSubmit={handleSubmit} className="flex flex-col gap-6 flex-1 mb-6">
-        <button className="w-max p-2 shadow-md rounded-xl text-sm text-gray-500 bg-white">
-          Add a cover image
-        </button>
+        <Upload type="image" setProgress={setProgress} setData={setCover}>
+          <button className="w-max p-2 shadow-md rounded-xl text-sm text-gray-500 bg-white">
+            Add a cover image
+          </button>
+        </Upload>
 
         <input
           className="text-4xl font-semibold bg-transparent outline-none"
@@ -70,18 +89,30 @@ const Write = () => {
           name="desc"
           placeholder="A Short Description"
         />
-        <ReactQuill
-          theme="snow"
-          className="flex-1 rounded-xl bg-white shadow-md"
-          value={value}
-          onChange={setValue}
-        />
+        <div className="flex flex-1 ">
+          <div className="flex flex-col gap-2 mr-2">
+            <Upload type="image" setProgress={setProgress} setData={setImg}>
+              🌆
+            </Upload>
+            <Upload type="video" setProgress={setProgress} setData={setVideo}>
+              ▶️
+            </Upload>
+          </div>
+          <ReactQuill
+            theme="snow"
+            className="flex-1 rounded-xl bg-white shadow-md"
+            value={value}
+            onChange={setValue}
+            readOnly={0 < progress && progress < 100}
+          />
+        </div>
         <button
-          disabled={mutation.isPending}
+          disabled={mutation.isPending || (0 < progress && progress < 100)}
           className="bg-blue-800 text-white font-medium rounded-xl mt-4 p-2 w-36 disabled:bg-blue-400 disabled:cursor-not-allowed"
         >
           {mutation.isPending ? "Loading..." : "Send"}
         </button>
+        {"Progress:" + progress}
         {mutation.isError && <span>{mutation.error.message}</span>}
       </form>
     </div>
